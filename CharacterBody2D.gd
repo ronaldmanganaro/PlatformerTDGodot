a088c4e48feda8b7c3bdada042fc98dee974e878
Ronald Manganaro <RonaldManganaro@gmail.com>
2024-06-16 07:54:39 -0400

    added assetts/animations started/ level

---
+++ CharacterBody2D.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animation = $AnimatedSprite2D
+@onready var anim_tree = $AnimationTree
+
+func _process(delta):
+	update_anim_state()
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		animation.play("Run")
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	
+func update_anim_state():
+	if(velocity == Vector2.ZERO):
+		anim_tree["parameters/conditions/is_idle"] = true
+		anim_tree["parameters/conditions.is_moving"] = false
+	else:
+		anim_tree["parameters/conditions/isidle"] = false
+		anim_tree["parameters/conditions.is_moving"] = true
